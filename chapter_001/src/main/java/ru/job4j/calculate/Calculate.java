ad3e40271320d22df9800ca0a145d1c3f349f5e0
Ivan Dvoeglazov <dvoeglazovcom@gmail.com>
2019-01-04 12:55:21 +0500

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,8 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+* Class Calculate
+* @author dvoeglazov
+* @since 04.01.2019
+*/
 public class Calculate {
-
- public static void main(String[] args) {
+/**
+* Метод мэйн
+* @param String[] args
+*/
+  public static void main(String[] args) {
   System.out.println("Hello World");
  }
 
