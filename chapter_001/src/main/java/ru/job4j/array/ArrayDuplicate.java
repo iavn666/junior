e08953cff486ec82bbf91206e114041846c5b679
Ivan Dvoeglazov <dvoeglazovcom@gmail.com>
2019-02-18 14:00:04 +0500

    6.8. Удаление дубликатов

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+
+import java.util.Arrays;
+
+public class ArrayDuplicate {
+
+    public String[] remove(String[] array) {
+        int dublicate = array.length;
+        for (int out = 0; out < dublicate; out++) {
+            for (int in = out + 1;in < dublicate; in++) {
+                if (array[out].equals(array[in]));
+                    array[out+1] = array[dublicate - 1];
+                    in--;
+                    dublicate--;
+                }
+        }
+        return Arrays.copyOf(array,dublicate);
+    }
+}
