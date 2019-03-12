1e772818a05d019c5de2725ec3102449c9b10f90
Ivan Dvoeglazov <dvoeglazovcom@gmail.com>
2019-03-12 23:11:55 +0500

    pravil

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -8,13 +8,14 @@ public class ArrayDuplicate {
     public String[] remove(String[] array) {
         int dublicate = array.length;
         for (int out = 0; out < dublicate; out++) {
-            for (int in = out + 1;in < dublicate; in++) {
-                if (array[out].equals(array[in]));
-                    array[out+1] = array[dublicate - 1];
+            for (int in = out + 1; in < dublicate; in++) {
+                if (array[out].equals(array[in])) {
+                    array[out + 1] = array[dublicate - 1];
                     in--;
                     dublicate--;
                 }
+            }
         }
-        return Arrays.copyOf(array,dublicate);
+        return Arrays.copyOf(array, dublicate);
     }
 }
