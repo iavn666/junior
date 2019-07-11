9368756fa53df9b94a1bb396e13b51f0a655b442
Ivan Dvoeglazov <dvoeglazovcom@gmail.com>
2019-07-11 13:07:24 +0500

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#34117][#158508]

--- chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
@@ -38,6 +38,20 @@ public class StubInput implements Input {
     }
 
     public int ask(String question, int[] range) {
-        return -1;
+        int key = Integer.valueOf(this.ask(question));
+        boolean exist = false;
+        for (int value : range) {
+            if (value == key) {
+                exist = true;
+                break;
+            }
+        }
+        if (exist) {
+            return key;
+        } else {
+            throw new MenuOutException("Out of menu renge");
+        }
     }
+
+
 }
