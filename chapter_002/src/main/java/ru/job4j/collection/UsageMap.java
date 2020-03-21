629742ff3a42045531b5c4cdc5bff88585bd7dbb
Ivan Dvoeglazov <dvoeglazovcom@gmail.com>
2020-03-22 01:46:08 +0500

    0. Map, HashMap [#212652]

---
+++ chapter_002/src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("ivan.vanya.2012@inox.ru", "Двоеглазов Иван Николаевич");
+        for (String key : map.keySet()) {
+            System.out.println(key + " " + map.get(key));
+        }
+
+    }
+}
