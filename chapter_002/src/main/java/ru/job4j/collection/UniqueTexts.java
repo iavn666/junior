5613b545c3f6d1f83cfe8b6a8131f2e49e798ce1
iavan666 <dvoeglazovcom@gmail.com>
2020-08-03 21:47:26 +0500

    0. Set, HashSet, Iterator. [#10095][#337970]

---
+++ chapter_002/src/main/java/ru/job4j/collection/UniqueTexts.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueTexts {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String num : origin) {
+            check.add(num);
+        }
+        for (String num : text) {
+            return check.contains(num);
+
+        }
+        /* for-each origin -> new HashSet. */
+        /* for-each text -> hashSet.contains */
+        return rsl;
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/collection/UniqueTextsTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class UniqueTextsTest {
+
+    @Test
+    public void isEquals() {
+        String origin = "My cat eats a mouse and milk";
+        String text = "My cat eats milk and a mouse";
+        assertThat(UniqueTexts.isEquals(origin, text), is(true));
+    }
+
+    @Test
+    public void isNotEquals() {
+        String origin = "My cat eats a mouse";
+        String text = "A mouse is eaten by a cat";
+        assertThat(UniqueTexts.isEquals(origin, text), is(false));
+    }
+}
