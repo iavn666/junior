bbab7232ba99e2befc0f76d94a4d6573251a32d3
iavan666 <dvoeglazovcom@gmail.com>
2020-08-04 07:32:43 +0500

    0. Map, HashMap [#212652]

--- chapter_002/src/main/java/ru/job4j/collection/UsageMaps.java
+++ chapter_002/src/main/java/ru/job4j/collection/UsageMaps.java
@@ -3,8 +3,9 @@ package ru.job4j.collection;
 import java.util.HashMap;
 
 public class UsageMaps {
-    public static void main (String[] args) {
-        HashMap<String,String> name = new HashMap<>();
+
+    public static void main(String[] args) {
+        HashMap<String, String> name = new HashMap<>();
         name.put("ivan.vanya.2012@inbox.ru", "Dvoeglazov Ivan");
         for (String num : name.keySet()) {
             String value = name.get(num);
