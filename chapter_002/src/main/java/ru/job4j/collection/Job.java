fbcbc4b74abc6dd1fc1673728df373127b6128ae
iavan666 <dvoeglazovcom@gmail.com>
2020-08-05 21:16:13 +0500

    2. Комбинированный компаратор [#10036]

---
+++ chapter_002/src/main/java/ru/job4j/collection/Job.java
@@ -0,0 +1,35 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class Job implements Comparator<Job> {
+    private String name;
+
+    private int priority;
+
+    public Job(String name, int priority) {
+        this.name = name;
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public String toString() {
+        return "Job{" + name + ", " + priority + '}';
+    }
+
+    @Override
+    public int compare(Job o1, Job o2) {
+        return 0;
+    }
+
+
+
+}
