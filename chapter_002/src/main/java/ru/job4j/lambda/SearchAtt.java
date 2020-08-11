a78ec19e53a28c16eb3026f4dc3fd7ca43f3bc4f
iavan666 <dvoeglazovcom@gmail.com>
2020-08-11 08:48:35 +0500

    2.0. Встроенные функциональные интерфейсы. [#249209][#341405]

--- chapter_002/src/main/java/ru/job4j/lambda/SearchAtt.java
+++ chapter_002/src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -1,7 +1,6 @@
 package ru.job4j.lambda;
 
 
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BinaryOperator;
@@ -9,19 +8,47 @@ import java.util.function.Predicate;
 
 public class SearchAtt {
 
-    public static List<Attachment> filterSize(List<Attachment> list) {
+    public static List<Attachment> filter(Predicate<Attachment> num) {
+        List<Attachment> res = new ArrayList<>();
+        filterSize(res);
+        filterName(res);
+        return res;
+    }
 
-        Predicate<Attachment> res = new Predicate<Attachment>() {
+    public static List<Attachment> filterSize(List<Attachment> list) {
+        List<Attachment> resultSize = new ArrayList<>();
+        Predicate<Attachment> num = new Predicate<Attachment>() {
             @Override
             public boolean test(Attachment attachment) {
                 for (Attachment att : list) {
                     if (att.getSize() > 100) {
-                        list.add(attachment);
+                        resultSize.add(att);
+                        return true;
                     }
                 }
-                return true;
+                return false;
             }
         };
-      return list;
+        return filter(num);
     }
+
+    public static List<Attachment> filterName(List<Attachment> list) {
+        List<Attachment> resultName = new ArrayList<>();
+        Predicate<Attachment> num = new Predicate<Attachment>() {
+            @Override
+            public boolean test(Attachment attachment) {
+                for (Attachment att : list) {
+                    if (att.getName().contains("bug")) {
+                        resultName.add(att);
+                        return true;
+                    }
+                }
+                return false;
+            }
+        };
+        return filter(num);
+    }
+
+
+
 }
