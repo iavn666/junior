8e580398b9921a3bde839bd7b99e13832c542f71
iavan666 <dvoeglazovcom@gmail.com>
2020-08-12 14:53:29 +0500

    2.0. Встроенные функциональные интерфейсы. [#249209][#341405]

--- chapter_002/src/main/java/ru/job4j/lambda/SearchAtt.java
+++ chapter_002/src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -9,47 +9,47 @@ import java.util.function.Predicate;
 public class SearchAtt {
 
 
-    public static List<Attachment> filter(Predicate<Attachment> num) {
-        List<Attachment> res = new ArrayList<>();
-        filterSize(res);
-        filterName(res);
-        return res;
+    public static List<Attachment> filter(Predicate<Attachment> num, List<Attachment> list) {
+        List<Attachment> result = new ArrayList<>();
+        for (Attachment att : list) {
+            if (num.test(att)) {
+                result.add(att);
+
+            }
+        }
+        return result;
     }
 
     public static List<Attachment> filterSize(List<Attachment> list) {
-        List<Attachment> resultSize = new ArrayList<>();
         Predicate<Attachment> num = new Predicate<Attachment>() {
             @Override
             public boolean test(Attachment attachment) {
-                for (Attachment att : list) {
-                    if (att.getSize() > 100) {
-                        resultSize.add(att);
-                        return true;
-                    }
+                boolean res = false;
+                if (attachment.getSize() > 100) {
+                    list.add(attachment);
+                    res = true;
                 }
-                return false;
+                return res;
             }
         };
-        return filter(num);
+        return filter(num, list);
+
     }
 
     public static List<Attachment> filterName(List<Attachment> list) {
-        List<Attachment> resultName = new ArrayList<>();
         Predicate<Attachment> num = new Predicate<Attachment>() {
             @Override
             public boolean test(Attachment attachment) {
-                for (Attachment att : list) {
-                    if (att.getName().contains("bug")) {
-                        resultName.add(att);
-                        return true;
-                    }
+                boolean res = false;
+                if (attachment.getName().contains("bug")) {
+                    list.add(attachment);
+                    res = true;
                 }
-                return false;
+                return res;
             }
         };
-        return filter(num);
+        return filter(num, list);
     }
-
 
 
 }
